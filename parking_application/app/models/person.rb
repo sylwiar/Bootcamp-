558ab6b2016ed627d4602da9b7b23292d16501d6
Sylwia Robak <sylwiar@sylwiar.(none)>
2016-07-06 20:14:30 +0200

    Add validations to Person

--- parking_application/app/models/person.rb
+++ parking_application/app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ActiveRecord::Base
+  validates :first_name, presence: true
+
   has_many :parkings
   has_many :cars
 end
--- parking_application/test/models/person_test.rb
+++ parking_application/test/models/person_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @person = people(:steve)
+  end
+
+  test 'person should be valid' do
+    assert @person.valid?
+  end
+
+  test 'should not save person without first_name' do
+    @person.first_name = nil
+    assert_not @person.valid?
+    assert @person.errors.has_key?(:first_name)
+  end
+
+  test 'should display full name' do
+    assert_equal("Steve Jobs", @person.full_name)
+  end
+
+  test "should display only first name" do
+    @person.last_name = nil
+    assert_equal("Steve", @person.full_name)
+  end
 end
