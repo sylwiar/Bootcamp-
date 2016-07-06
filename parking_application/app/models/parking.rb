879773590fd0acf04dd0ae7998e937ed1500ce62
Sylwia Robak <sylwiar@sylwiar.(none)>
2016-07-06 23:40:41 +0200

    Add validations to Parking

--- parking_application/app/models/parking.rb
+++ parking_application/app/models/parking.rb
@@ -1,4 +1,8 @@
-class PlaceRent < ActiveRecord::Base
+class Parking < ActiveRecord::Base
+  validates :places, presence: true
+  validates :hour_price, :day_price, presence: true, numericality: true
+  validates :kind, inclusion: { in: %w(outdoor indoor private street) }
+
   belongs_to :address
   belongs_to :owner, class_name: "Person"
   has_many :place_rents
--- parking_application/test/models/parking_test.rb
+++ parking_application/test/models/parking_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
-class PlaceRentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class ParkingTest < ActiveSupport::TestCase
+  def setup
+    @parking = parkings(:renoma)
+  end
+
+  test 'parking should be valid' do
+    assert @parking.valid?
+  end
+
+  test 'should not save parking without places' do
+    @parking.registration_number = nil
+    assert_not @parking.valid?
+    assert @parking.errors.has_key?(:places)
+  end
+
+  test 'should not save parking without hour_price' do
+    @parking.model = nil
+    assert_not @parking.valid?
+    assert @parking.errors.has_key?(:hour_price)
+  end
+
+  test 'should not save parking without day_price' do
+    @parking.owner = nil
+    assert_not @parking.valid?
+    assert @parking.errors.has_key?(:day_price)
+  end
+
+  test 'should not save parking without kind' do
+    @parking.owner = nil
+    assert_not @parking.valid?
+    assert @parking.errors.has_key?(:kind)
+  end
+
+  test "should have numeric hour_price" do
+    @parking.hour_price = "test"
+    assert_not @parking.valid?
+    assert @parking.errors.has_key?(:hour_price)
+  end
+
+  test "kind should have value from list" do
+    @parking.kind = "something"
+    assert @parking.invalid?
+  end
 end
