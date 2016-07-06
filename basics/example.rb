43bbb063306cf936aa13065f024a3e4809d75adf
Sylwia Robak <sylwiar@sylwiar.(none)>
2016-07-06 12:54:39 +0200

    Create constructor

--- basics/example.rb
+++ basics/example.rb
@@ -1,4 +1,13 @@
 class Article 
   attr_reader :title, :body, :author, :created_at
   attr_accessor :likes, :dislikes
+
+  def initialize(title, body, author = nil)
+    @title = title
+    @body = body
+    @author = author
+    @created_at = Time.now
+    @likes = 0
+    @dislikes = 0
+  end
 end
