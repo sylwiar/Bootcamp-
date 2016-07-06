f0f5df38fe9316c34fdbaa65d84d2d191e9360ac
Sylwia Robak <sylwiar@sylwiar.(none)>
2016-07-06 23:27:24 +0200

    Add validations to PlaceRent

--- parking_application/app/models/place_rent.rb
+++ parking_application/app/models/place_rent.rb
@@ -1,4 +1,6 @@
 class PlaceRent < ActiveRecord::Base
+  validates :start_date, :end_date, :car, presence: true
+
   belongs_to :parking
   belongs_to :car
   has_many :people
--- parking_application/test/models/place_rent_test.rb
+++ parking_application/test/models/place_rent_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class PlaceRentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @place_rent = place_rents(:bmw_at_renoma)
+  end
+
+  test 'place_rent should be valid' do
+    assert @place_rent.valid?
+  end
+
+  test 'should not save place_rent without start_date' do
+    @place_rent.registration_number = nil
+    assert_not @place_rent.valid?
+    assert @place_rent.errors.has_key?(:start_date)
+  end
+
+  test 'should not save place_rent without end_date' do
+    @place_rent.model = nil
+    assert_not @place_rent.valid?
+    assert @place_rent.errors.has_key?(:end_date)
+  end
+
+  test 'should not save place_rent without car' do
+    @place_rent.owner = nil
+    assert_not @place_rent.valid?
+    assert @place_rent.errors.has_key?(:car)
+  end
+
+  test 'should not save place_rent without parking' do
+    @place_rent.owner = nil
+    assert_not @place_rent.valid?
+    assert @place_rent.errors.has_key?(:parking)
+  end
 end
