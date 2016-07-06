8e0fd7a50fb9674d7c8d7b32b2a93213c60fa89f
Sylwia Robak <sylwiar@sylwiar.(none)>
2016-07-06 19:51:31 +0200

    Continuation of validations to Address- tests

--- parking_application/test/models/address_test.rb
+++ parking_application/test/models/address_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @address = addresses(:wroclaw)
+  end
+
+  test 'address should be valid' do
+    assert @address.valid?
+  end
+
+  test 'should not save address without city' do
+    @address.city = nil
+    assert_not @address.valid?
+    assert @address.errors.has_key?(:city)
+  end
+
+  test 'should not save address without street' do
+    @address.street = nil
+    assert_not @address.valid?
+    assert @address.errors.has_key?(:street)
+  end
+
+  test 'should not save address without zip_code' do
+    @address.zip_code = nil
+    assert_not @address.valid?
+    assert @address.errors.has_key?(:zip_code)
+  end
+
+  test 'should not save address with wrong zip code' do
+    @address.zip_code = '123456'
+    assert_not @address.valid?
+    assert @address.errors.has_key?(:zip_code)
+  end
 end
