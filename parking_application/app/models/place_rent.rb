573d4865d584c661a1e00b0375baf2e56f99033d
Sylwia Robak <sylwiar@sylwiar.(none)>
2016-07-07 15:14:18 +0200

    Calculate price before PlaceRent is saved

--- parking_application/app/models/place_rent.rb
+++ parking_application/app/models/place_rent.rb
@@ -1,7 +1,14 @@
 class PlaceRent < ActiveRecord::Base
+  before_save :calculate_price
   validates :start_date, :end_date, :car, presence: true
 
   belongs_to :parking
   belongs_to :car
   has_many :people
+
+  def calculate_price
+    day_spent = (end_date - start_date).to_i/3600*24
+    hour_spent = (end_date - start_date).to_i%3600*24
+    self.price = parking.day_price * day_spent + parking.hour_price * hour_spent
+  end
 end
