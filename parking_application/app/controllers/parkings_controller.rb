b7f219f725d1c25c6f26b5c8a6d4147f685919f9
Sylwia Robak <sylwiar@sylwiar.(none)>
2016-07-07 11:09:46 +0200

    Add show and create action (parking)

--- parking_application/app/controllers/parkings_controller.rb
+++ parking_application/app/controllers/parkings_controller.rb
@@ -2,4 +2,28 @@ class ParkingsController < ApplicationController
   def index
     @parkings = Parking.all
   end
+
+  def show
+    @parking = Parking.find(params[:id])
+  end
+
+  def new
+    @parking = Parking.new
+  end
+
+  def create
+    @parking = Parking.new(parking_params)
+ 
+    if @parking.save
+      redirect_to @parking
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def parking_params
+    params.require(:parking).permit(:places, :kind, :hour_price, :day_price, :address, :owner)
+  end
 end
--- parking_application/app/models/parking.rb
+++ parking_application/app/models/parking.rb
@@ -1,7 +1,9 @@
 class Parking < ActiveRecord::Base
+  kinds = %w(outdoor indoor private street)
+
   validates :places, presence: true
   validates :hour_price, :day_price, presence: true, numericality: true
-  validates :kind, inclusion: { in: %w(outdoor indoor private street) }
+  validates :kind, inclusion: { in: kinds }
 
   belongs_to :address
   belongs_to :owner, class_name: "Person"
