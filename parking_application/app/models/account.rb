434b1e70ac08ee840ea0e130f61009b086dc2dd9
Sylwia Robak <sylwiar@sylwiar.(none)>
2016-07-12 23:02:34 +0200

    Add Account methods

--- parking_application/app/models/account.rb
+++ parking_application/app/models/account.rb
@@ -3,4 +3,18 @@ class Account < ActiveRecord::Base
   belongs_to :person
 
   accepts_nested_attributes_for :person
+
+  PEPPER = '9782901ee15a5651b9f5'
+  STRETCHES = 10
+
+  def valid_password?(password)
+    return false if encrypted_password.blank?
+    bcrypt = ::BCrypt::Password.new(encrypted_password)
+    password = ::BCrypt::Engine.hash_secret("#{password}#{PEPPER}", bcrypt.salt)
+    password == encrypted_password
+  end
+
+  def password_digest(password)
+    ::BCrypt::Password.create("#{password}#{PEPPER}", :cost => STRETCHES).to_s
+  end
 end
