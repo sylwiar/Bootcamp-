853030541855cb22fcb728fae2b3ad6843adfe5d
Sylwia Robak <sylwiar@sylwiar.(none)>
2016-07-12 22:08:15 +0200

    Tests to account

--- parking_application/test/models/account_test.rb
+++ parking_application/test/models/account_test.rb
@@ -1,45 +1,23 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  test 'it should belong to person' do
+  def setup
+    @account = accounts(:one)
   end
 
-  test 'it should create person object (and try to guess name) when saving' do
+  test 'account should be valid' do
+    assert @account.valid?
   end
 
-  test 'it should be invalid without email' do
+  test 'should not save account without email' do
+    @account.email = nil
+    assert_not @account.valid?
+    assert @account.errors.has_key?(:email)
   end
 
-  test 'it should be invalid without password on create' do
-  end
-
-  test 'it should be valid without password after create' do
-  end
-
-  test 'encrypted password should be created when setting password' do
-  end
-
-  test 'it should be invalid with wrong password confirmation' do
-  end
-
-  test 'password comparation should return false when account does not have encrypted password at all' do
-  end
-
-  test 'password comparation should return false when password is different' do
-  end
-
-  test 'password comparation should return true when password is the same' do
-  end
-
-  test 'Account.authenticate should return account object if email and password are valid' do
-  end
-
-  test 'Account.authenticate should return nil if password does not match' do
-  end
-
-  test 'Account.authenticate should return nil if email can not be found' do
-  end
-
-  test 'Account.authenticate! should raise exception if authentication can not be done' do
+  test 'when account has no valid email' do
+    @account.email = 'mail@mail,+com.'
+    assert_not @account.valid?
+    assert @account.errors.has_key?(:email)
   end
 end
